221ce95c148460b671934a0edfa35c61ce04e1c8
sushant <sushantsonawane121@gmail.com>
2022-09-06 11:53:50 +0530

    final commit

--- js/script.js
+++ js/script.js
@@ -25,3 +25,28 @@ var typed = new Typed(".auto-type",{
 function message(){
     alert("under development");
 }
+
+
+
+
+
+// back to top btn
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
