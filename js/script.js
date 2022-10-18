d64ad3c8bb012470b9e296bdbfc67df4c6263cc5
sushant <sushantsonawane121@gmail.com>
2022-10-18 19:16:56 +0530

    progress bar add

--- js/script.js
+++ js/script.js
@@ -51,3 +51,34 @@ var mybutton = document.getElementById("myBtn");
 //   document.body.scrollTop = 0;
 //   document.documentElement.scrollTop = 0;
 // }
+
+
+// progress bar scrit
+
+
+// let circularProgress = document.querySelector(".progress"),
+// progressValue = document.querySelector(".progress-value");
+
+// let progressStartValue = 0,    
+// progressEndValue = 90,    
+// speed = 50;
+
+// let progress = setInterval(() => {
+// progressStartValue++;
+
+// progressValue.textContent = `${progressStartValue}%`
+// circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+// if(progressStartValue == progressEndValue){
+//     clearInterval(progress);
+// }    
+// }, speed);
+
+
+document.querySelector(".progress").style.background = `conic-gradient(#7d2ae8 ${90 * 3.6}deg, #ededed 0deg)`;
+document.querySelector(".progress-js").style.background = `conic-gradient(#7d2ae8 ${50 * 3.6}deg, #ededed 0deg)`;
+document.querySelector(".progress-node").style.background = `conic-gradient(#7d2ae8 ${20 * 3.6}deg, #ededed 0deg)`;
+document.querySelector(".progress-react").style.background = `conic-gradient(#7d2ae8 ${20 * 3.6}deg, #ededed 0deg)`;
+document.querySelector(".progress-mongo").style.background = `conic-gradient(#7d2ae8 ${50 * 3.6}deg, #ededed 0deg)`;
+document.querySelector(".progress-exp").style.background = `conic-gradient(#7d2ae8 ${5 * 3.6}deg, #ededed 0deg)`;
+
